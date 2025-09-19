a4d12ac3fbf63f76d119182321d9c19eedfdf00c
roxypoceros <rox_ghp@hotmail.com>
2025-09-19 02:17:37 -0600

    TarjetaCredito class ready

--- src/proyectoMexBank/TarjetaCredito.java
+++ src/proyectoMexBank/TarjetaCredito.java
@@ -8,6 +8,23 @@ package proyectoMexBank;
  *
  * @author rox_g
  */
-public class TarjetaCredito {
+// Extender clase TarjetaCredito de CuentaBase
+public class TarjetaCredito extends CuentaBase {
+    
+    //Agregar constructor
+    TarjetaCredito(double apertura){
+        // Con super llama al constructor de CuentaBase
+        // Asigna saldo negativo
+        super(-apertura);
+    }
+    
+    // Método sumaIntereses para sumar un 15% de interés a la deuda actual
+    public void sumaIntereses(){
+        double deudaActual = getMontoActual();
+        double interes = deudaActual * 0.15;
+        double nuevaDeuda = deudaActual + interes;
+        setMontoActual(nuevaDeuda);
+    }
     
 }
+
