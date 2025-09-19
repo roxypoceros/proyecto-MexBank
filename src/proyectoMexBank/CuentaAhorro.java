5b463b6785c9726d3833d7c2500181e139c7127e
roxypoceros <rox_ghp@hotmail.com>
2025-09-19 02:41:04 -0600

    TarjetaAhorro class ready

---
+++ src/proyectoMexBank/CuentaAhorro.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoMexBank;
+
+/**
+ *
+ * @author rox_g
+ */
+// Extender clase TarjetaCredito de CuentaBase
+public class CuentaAhorro extends CuentaBase {
+    //Agregar constructor
+    CuentaAhorro(double apertura){
+        // Iniciar con apertura como saldo inicial
+        super(apertura);
+    }
+    
+    //Método invertir sumará 10% del montoActual al montoActual
+    public void invertir(){
+        double saldoActual = getMontoActual();
+        double interes10 = saldoActual * 0.10;
+        double nuevoSaldo = saltoActual + interes10;
+        //Actualizar el saldo con el nuevoSaldo
+        setMontoActual(nuevoSaldo);
+    }
+    
+  
+}
